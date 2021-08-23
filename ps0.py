291f0729eb56e64d3400e06316cedd18bcdd05f4
AbdElRahmanElGharib <68001410+AbdElRahmanElGharib@users.noreply.github.com>
2021-08-24 00:02:39 +0200

    Add files via upload

---
+++ ps0.py
@@ -0,0 +1,24 @@
+
+import numpy
+import readchar
+
+
+while True:
+
+    try:
+        n = float(input('Enter n:'))
+        x = float(input('Enter the power:'))
+        break
+    except:
+        print('---Enter a valid NUMBER---')
+    pass
+
+
+result_1 = n**x
+result_2 = numpy.log2(n)
+
+print('n**x =',result_1)
+print('log2 of n =',result_2)
+
+print('/n Press any key to exit')
+k = readchar.readchar()
